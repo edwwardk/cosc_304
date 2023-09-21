085fa62e4207227d78110051fb3cbe3b0f8837bb
Edward <eddykneller@gmail.com>
2023-09-21 13:54:31 -0700

    lab2q1 almost complete

--- labs/lab2/assign/cosc304lab2q1.sql
+++ labs/lab2/assign/cosc304lab2q1.sql
@@ -1,23 +1,23 @@
-DROP TABLE IF EXISTS Airplane;
-DROP TABLE IF EXISTS Airport;
-DROP TABLE IF EXISTS Flight;
-DROP TABLE IF EXISTS Passenger;
-DROP TABLE IF EXISTS OnFlight;
+DROP TABLE IF EXISTS Airplane CASCADE;
+DROP TABLE IF EXISTS Airport CASCADE;
+DROP TABLE IF EXISTS Flight CASCADE;
+DROP TABLE IF EXISTS Passenger CASCADE;
+DROP TABLE IF EXISTS OnFlight CASCADE;
 
 CREATE TABLE Airplane (
-	planeId VARCHAR(10),
+	planeID VARCHAR(10),
 	planeModel VARCHAR(20),
 	planeManDate DATE,
 	PRIMARY KEY (planeId)
 );
 
 CREATE TABLE Airport (
-	airportId CHAR(5),
+	airportID CHAR(5),
 	airportName VARCHAR(30),
 	airportCity VARCHAR(40),
 	airportProvine VARCHAR(20),
 	airportCountry VARCHAR(20),
-	PRIMARY KEY (airportId)
+	PRIMARY KEY (airportID)
 );
 
 CREATE TABLE Flight (
@@ -27,8 +27,38 @@ CREATE TABLE Flight (
 	arriveAirport CHAR(5),
 	expectedArriveDateTime DATETIME,
 	actualArriveDateTime DATETIME,
-	planeId VARCHAR(10),
-	FOREIGN KEY (planeId) REFERENCES Airplane(planeId)
+	planeID VARCHAR(10),
+	PRIMARY KEY (flightNumber, departDate),
+	FOREIGN KEY (planeId) REFERENCES Airplane(planeID)
 		ON DELETE SET NULL
+		ON UPDATE CASCADE
+);
+
+CREATE TABLE Passenger (
+	passID INTEGER,
+	passFirstName VARCHAR(30),
+	passLastName VARCHAR(30),
+	passBday DATE,
+	passStreet VARCHAR(50),
+	passCity VARCHAR(40),
+	passProvince VARCHAR(20),
+	passCountry VARCHAR(20)
+	PRIMARY KEY (passID)
+);
+
+CREATE TABLE OnFlight (
+	passID INTEGER,
+	flightNumber CHAR(5),
+	departDate DATETIME,
+	seatNumber CHAR(4),
+	PRIMARY KEY (seatNumber),
+	FOREIGN KEY (passID) REFERENCES Passenger(passID)
+		ON DELETE NO ACTION
+		ON UPDATE CASCADE,
+	FOREIGN KEY (flightNumber) REFERENCES Flight(flightNumber)
+		ON DELETE NO ACTION
+		ON UPDATE CASCADE,
+	FOREIGN KEY (departDate) REFERENCES Flight(departDate)
+		ON DELETE NO ACTION
 		ON UPDATE CASCADE
 );
