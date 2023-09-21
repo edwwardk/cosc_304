d372b83f9960e4373b783bf8816b85486268c8bd
Edward <eddykneller@gmail.com>
2023-09-21 13:37:27 -0700

    q1 fin airplane, airport, flight

--- labs/lab2/assign/cosc304lab2q1.sql
+++ labs/lab2/assign/cosc304lab2q1.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS Airplane;
+DROP TABLE IF EXISTS Airport;
+DROP TABLE IF EXISTS Flight;
+DROP TABLE IF EXISTS Passenger;
+DROP TABLE IF EXISTS OnFlight;
+
 CREATE TABLE Airplane (
 	planeId VARCHAR(10),
 	planeModel VARCHAR(20),
@@ -10,12 +16,19 @@ CREATE TABLE Airport (
 	airportName VARCHAR(30),
 	airportCity VARCHAR(40),
 	airportProvine VARCHAR(20),
-	airportCountry VARCHAR(20)
+	airportCountry VARCHAR(20),
+	PRIMARY KEY (airportId)
 );
 
 CREATE TABLE Flight (
 	flightNumber CHAR(5),
 	departDate DATETIME,
 	departAirport CHAR(5),
-	arriveAirport CHAR(5)
+	arriveAirport CHAR(5),
+	expectedArriveDateTime DATETIME,
+	actualArriveDateTime DATETIME,
+	planeId VARCHAR(10),
+	FOREIGN KEY (planeId) REFERENCES Airplane(planeId)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
 );
